53ebeda5c9fdb3709c002a1cbab444a5e1d2669f
Ini10000 <iniobong10000@gmail.com>
2022-01-04 02:13:33 +0100

    removed unnecessary pages

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -6,15 +6,12 @@ import RequireAuth from "../components/RequireAuth";
 import Login from "../screens/Login";
 import LandingPageGuest from "../screens/LandingPageGuest";
 import Admin from "../screens/LandingPageAdmin";
-import Apply from "../screens/JobAppForm";
-import Create from "../screens/JobCreateForm";
 import Error from "../screens/Error";
 
 export default function main() {
   return (
     <Routes>
       <Route path={ROUTES.HOME} element={<LandingPageGuest />} />
-      <Route path={ROUTES.JOB_APPLY} exact element={<Apply />} />
       <Route path={ROUTES.LOGIN} element={<Login />} />
       <Route
         path={ROUTES.ADMIN_HOME}
@@ -22,15 +19,6 @@ export default function main() {
         element={
           <RequireAuth redirectTo={ROUTES.LOGIN}>
             <Admin />
-          </RequireAuth>
-        }
-      />
-      <Route
-        path={ROUTES.CREATE_JOB}
-        exact
-        element={
-          <RequireAuth redirectTo={ROUTES.LOGIN}>
-            <Create />
           </RequireAuth>
         }
       />

08a02a749a3ef501fbd2e6a33bb8616676add4a3
Ini-obong Ukpong <iniobong10000@gmail.com>
2022-01-05 09:30:43 +0100

    Update RightNav.jsx

--- src/components/RightNav.jsx
+++ src/components/RightNav.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-import CustomButton from "../components/Button";
 import * as ROUTES from "../routes/routes";
 
 const Ul = styled.ul`
